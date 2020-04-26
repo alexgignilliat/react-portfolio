203e88e0eed1292e884fbd3a3e2e56222ea27c22
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-04-26 13:50:08 -0400

    Updates

---
+++ precache-manifest.fdc13560c25f8b1e36aefc283c68e386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89cf175ed982e7f9838f72861ff749b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e814ffdbc12e8df718b3",
+    "url": "/react-portfolio/static/css/main.a1d17366.chunk.css"
+  },
+  {
+    "revision": "4a18e330c6f8c63784f2",
+    "url": "/react-portfolio/static/js/2.a3a3c1dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.a3a3c1dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e814ffdbc12e8df718b3",
+    "url": "/react-portfolio/static/js/main.d1b35f6b.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  }
+]);
